4cc7792d28422e57be9ff8a356a0bcc87fe67898
fiorentini <kalebi.oliveira@hotmail.com>
2021-02-10 00:47:11 -0300

    News

---
+++ 1004/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _1004
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"PROD = {Convert.ToInt32(Console.ReadLine()) * Convert.ToInt32(Console.ReadLine())}");
+        }
+    }
+}

c8c5491616ba8454615b7f4331490f77f45057a7
fiorentini <kalebi.oliveira@hotmail.com>
2021-02-10 00:30:11 -0300

    Solving new problems

---
+++ 1002/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _1002
+{
+    class Program
+    {
+        // NÃO RESOLVIDO
+        static void Main(string[] args)
+        {
+            decimal pi = 3.14159M;
+            decimal raio = Convert.ToDecimal(Console.ReadLine().Replace('.',','));
+
+            decimal area = pi * raio * raio;
+
+            string result = Math.Round(area, 4).ToString().Replace(',', '.');
+
+            Console.WriteLine($"A={result}"); 
+        }
+    }
+}
---
+++ 1003/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _1003
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = Convert.ToInt32(Console.ReadLine());
+            var b = Convert.ToInt32(Console.ReadLine());
+
+            var test = a + b;
+
+            Console.WriteLine($"SOMA = {test}");
+        }
+    }
+}

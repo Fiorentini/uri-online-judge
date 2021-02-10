79b369158accf305afbbc1ffe362620cfdafe189
fiorentini <kalebi.oliveira@hotmail.com>
2021-02-09 23:34:12 -0300

    init

---
+++ 1001/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _1001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = Console.ReadLine();
+            var b = Console.ReadLine();
+
+            var x = Convert.ToInt32(a) + Convert.ToInt32(b);
+
+            Console.WriteLine($"X = {x}");
+        }
+    }
+}
